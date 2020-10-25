e0f9ddd991570dd9a7faecb9f41ba80e66f21a3e
elisabeth <elisabeth.r2ni@gmail.com>
2020-10-25 19:31:54 +0200

    Add database class

---
+++ upt/database.py
@@ -0,0 +1,29 @@
+from contextlib import contextmanager
+
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+
+class Database:
+    @classmethod
+    @contextmanager
+    def connect(_, data_source_name: str):
+        connection = psycopg2.connect(dsn=data_source_name)
+
+        try:
+            yield connection
+            connection.commit()
+        except Exception:
+            connection.rollback()
+            raise
+        finally:
+            connection.close()
+
+    @classmethod
+    @contextmanager
+    def create_cursor(cls, connection):
+        try:
+            cursor = connection.cursor(cursor_factory=RealDictCursor)
+            yield cursor
+        finally:
+            cursor.close()
