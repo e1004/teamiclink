caedda9220f675038cd2c88d7eea239ef0eac5d0
elisabeth <elisabeth.r2ni@gmail.com>
2020-12-14 22:48:46 +0200

    Load logging config

--- teamiclink/start.py
+++ teamiclink/start.py
@@ -1,17 +1,25 @@
-from teamiclink.config import AppConfig
-from teamiclink.slack.store_install import TeamiclinkInstallStore
-from slack_bolt.adapter.flask import SlackRequestHandler
-from slack_bolt import App
-from teamiclink.slack.oauth_flow import TeamiclinkOAuth
-from slack_bolt.oauth.oauth_settings import OAuthSettings
-from teamiclink.slack.store_state import RedisOAuthStateStore
-from teamiclink.app import create_app
-from redis import Redis
+from logging.config import dictConfig
+
 import psycopg2.extras
+import yaml
+from redis import Redis
+from slack_bolt import App
+from slack_bolt.adapter.flask import SlackRequestHandler
+from slack_bolt.oauth.oauth_settings import OAuthSettings
+
+from teamiclink.app import create_app
+from teamiclink.config import AppConfig
+from teamiclink.slack.oauth_flow import TeamiclinkOAuth
+from teamiclink.slack.store_install import TeamiclinkInstallStore
+from teamiclink.slack.store_state import RedisOAuthStateStore
 
 psycopg2.extras.register_uuid()
 
 config = AppConfig.load_from_env()
+
+with open(config.path_logging_config, mode="r") as logging_config:
+    dictConfig(yaml.safe_load(logging_config.read()))
+
 
 installation_store = TeamiclinkInstallStore(data_source_name=config.dsn)
 slack_handler = SlackRequestHandler(
